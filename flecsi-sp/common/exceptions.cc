5bce6b4b88a149352925c0398ae3b603a912c0e1
HyeongKae Park <hkpark@lanl.gov>
2017-11-14 09:08:53 -0700

    moving specialization_init from apps to flecsi-sp
    These files performs mesh partitioning and they seems to be used
    by multiple apps. So it should be located here.

---
+++ flecsi-sp/common/exceptions.cc
@@ -0,0 +1,32 @@
+/*~-------------------------------------------------------------------------~~*
+ * Copyright (c) 2016 Los Alamos National Laboratory, LLC
+ * All rights reserved
+ *~-------------------------------------------------------------------------~~*/
+////////////////////////////////////////////////////////////////////////////////
+/// \file
+/// \brief Some utilities for exception handling.
+////////////////////////////////////////////////////////////////////////////////
+
+
+// system includes
+#ifdef _GNU_SOURCE
+#  include <fenv.h>
+#endif
+
+namespace apps {
+namespace common {
+
+///////////////////////////////////////////////////////////////////////////////
+// enable exceptions
+///////////////////////////////////////////////////////////////////////////////
+void enable_exceptions(void) {
+
+  // enable exceptions
+#if defined(_GNU_SOURCE) && !defined(NDEBUG)
+  feenableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
+#endif
+
+}
+
+} // namespace
+} // namespace
